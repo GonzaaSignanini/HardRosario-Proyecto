c373d42befab887b22c4dbd9daabc2cdbc839792
Gonzalo Signanini <gonzasignanini@hotmail.com>
2021-08-24 16:18:47 -0300

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+//   const objeto = {
+//     tipo: 'vehiculo',
+//     marca: 'BMW',
+//     modelo: 'X3',
+//     año: '2017'
+//   }
+// console.log(objeto)
+//   const nuevoObj = {
+//     ...objeto,
+//     año: '2019'
+//   }
+
+//   console.log(nuevoObj)
+
+  
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
